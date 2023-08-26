70397d8c13b3ac8dd1ab03843dee595f7b41fbb9
Bodarnn <103896552+Bodarnn@users.noreply.github.com>
2023-08-26 03:45:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,14 @@
-/*
- _____           _____ _    _ ____   ____          _____  _____  
-|  __ \   /\    / ____| |  | |  _ \ / __ \   /\   |  __ \|  __ \ 
-| |  | | /  \  | (___ | |__| | |_) | |  | | /  \  | |__) | |  | |
-| |  | |/ /\ \  \___ \|  __  |  _ <| |  | |/ /\ \ |  _  /| |  | |
-| |__| / ____ \ ____) | |  | | |_) | |__| / ____ \| | \ \| |__| |
-|_____/_/    \_\_____/|_|  |_|____/ \____/_/    \_\_|  \_\_____/ 
-
-*/
-
-let bert;
-let ernie;
-
-async function parse() {
-	const response = await fetch("data.csv");
-	const data = await response.text();
-	Papa.parse(data, {
-		dynamicTyping: true,
-		header: true,
-		complete: function (results, _) {
-			bert = Birds.fromCSV(results.data);
-			ernie = bert.copy();
-			dropdowns();
-			charts();
-		}
-	});
-}
-
-console.log("Working");
-window.addEventListener("load", parse);
+let global;
+let first;
+let filters = [];
+let second;
 
 function reset() {
-	bert = ernie.copy();
-	charts();
+	filters = [];
+	updateCharts();
 }
 
-function dropdowns() {
-	const ids = ["start-date", "end-date"];
-	const dates = bert.unique(["Date"]);
-	for (const id of ids) {
-		const select = document.getElementById(id);
-		for (const date of dates) {
-			const option = document.createElement("option");
-			option.value = date;
-			option.text = date;
-			select.add(option);
-		}
-		select.selectedIndex = 0;
-	}
-} // TODO: end-date selected index -> max
-
-function charts() {
-	const start = document.getElementById("start-date").value;
-	const end = document.getElementById("end-date").value;
-	const filteredData = bert.filterby(row => {
-		return row["Date"] >= start && row["Date"] <= end;
-	});
-
-	commanderBar(filteredData);
-	playerBar(filteredData);
-	archetypeCharts(filteredData);
-	colorCharts(filteredData);
-}
-
-function snackbar(val) {
+function displaySnackbar(val) {
 	const snackbar = document.getElementById("snackbar");
 	snackbar.className = "show";
 	snackbar.innerHTML = `Filtering by ${val}`;
@@ -72,35 +18,93 @@ function snackbar(val) {
 	}, 3000);
 }
 
-function commanderBar(data) {
-	let elmo = data.filterby(row => row["Player"] !== "Winner");
-	let abby = data.filterby(row => row["Player"] === "Winner");
+async function parse() {
+	const response = await fetch("data.csv");
+	const data = await response.text();
+	Papa.parse(data, {
+		dynamicTyping: true,
+		header: true,
+		complete: initialize
+	});
+}
+window.addEventListener("load", parse);
 
-	let bigBird = new Birds({});
-	bigBird["Commander"] = elmo.unique(["Commander"]);
-	bigBird["# Games"] = bigBird["Commander"].map(value => elmo.countif(["Commander"], val => val === value));
-	bigBird["# Wins"] = bigBird["Commander"].map(value => abby.countif(["Commander"], val => val === value));
-	bigBird["# Losses"] = bigBird.subtract(["# Games", "# Wins"]);
-	bigBird["Win %"] = bigBird.divide(['# Wins', '# Games']);
+function initialize(results, file) {
+	const data = results.data;
+	global = new Bird(data);
+	updateDropdowns();
+	updateCharts();
+}
 
-	// pattern = /^(.*?)(?=\s\/\/|,).*(?<=\/\/\s)(.*?)(?=,|$).*$/;
-	// replacement = "$1 // $2";
-	// bert["Commander"] = bert.replace(["Commander"], pattern, replacement);
+function updateDropdowns() {
+	const dates = global["Date"].unique();
+	const startElement = document.getElementById("start-date");
+	const endElement = document.getElementById("end-date");
+	for (const date of dates) {
+		const startOption = document.createElement("option");
+		startOption.value = date;
+		startOption.text = date;
+		startElement.add(startOption);
+		const endOption = document.createElement("option");
+		endOption.value = date;
+		endOption.text = date;
+		endElement.add(endOption);
+	}
+	startElement.selectedIndex = 0;
+	endElement.selectedIndex = dates.length - 1;	
+}
 
-	bigBird = bigBird.sortby("Commander", -1)
-	bigBird = bigBird.sortby("# Games", 1);
-	bigBird = bigBird.sortby("# Wins", 1);
-	bigBird = bigBird.tail(15);
+function updateCharts() {
+	const startDate = document.getElementById("start-date").value;
+	const endDate = document.getElementById("end-date").value;
+	first = global.filterby(row => row["Date"] >= startDate && row["Date"] <= endDate);
+	const n = filters.length;
+	if (n === 0) {
+		second = first;
+	}
+	if (n > 0) {
+		var col = filters[0][0];
+		var val = filters[0][1];
+		second = first.filterby(row => row[col] === val);
+	} 
+	if (n > 1) {
+		for (let i = 1; i < n; i++) {
+			const filter = filters[i];
+			var col = filter[0];
+			var val = filter[1];
+			second = second.filterby(row => row[col] === val);
+		}
+	}
+	updateCommanderBar();
+	updatePlayerBar();
+	updateArchetypeCharts();
+	updateColorCharts();
+}
 
-	maxGames = bigBird.max(["# Games"]);
-
+function updateCommanderBar() {
+	let playerData = second.filterby(row => row["Player"] !== "Winner");
+	let winnerData = second.filterby(row => row["Player"] === "Winner");
+	let bird = new Bird();
+	bird["Commander"] = playerData["Commander"].unique();
+	bird["# Games"] = bird["Commander"].map(x => playerData["Commander"].countif(y => y === x));
+	bird["# Wins"] = bird["Commander"].map(x => winnerData["Commander"].countif(y => y === x));
+	bird["# Losses"] = bird.sub(["# Games", "# Wins"]);
+	bird["Win %"] = bird.div(["# Wins", "# Games"]);
+	bird = bird.sortby("Commander", -1);
+	bird = bird.sortby("# Games", 1);
+	bird = bird.sortby("# Wins", 1);
+	bird = bird.tail(15);
+	const pattern = /^(.*?)(?=\s\/\/|,).*(?<=\/\/\s)(.*?)(?=,|$).*$/;
+	const replacement = "$1 // $2";
+	bird["Commander Repl"] = bird["Commander"].replace(pattern, replacement);
+	const maxGames = bird["# Games"].max();
 	Plotly.newPlot("commander-bar", [{
 		type: "bar",
-		x: bigBird["# Wins"],
-		y: bigBird["Commander"],
+		x: bird["# Wins"].toArray(),
+		y: bird["Commander Repl"].toArray(),
 		name: "# Wins",
 		hovertemplate: "%{y}<br>%{x} (%{customdata:.1%})<extra></extra>",
-		customdata: bigBird["Win %"],
+		customdata: bird["Win %"].toArray(),
 		orientation: "h",
 		marker: {
 			color: "rgb(3, 155, 229)"
@@ -112,14 +116,14 @@ function commanderBar(data) {
 		}
 	}, {
 		type: "bar",
-		x: bigBird["# Losses"],
-		y: bigBird["Commander"],
+		x: bird["# Losses"].toArray(),
+		y: bird["Commander Repl"].toArray(),
 		name: "# Losses",
-		text: bigBird["Win %"],
+		text: bird["Win %"].toArray(),
 		textposition: "outside",
 		texttemplate: "%{customdata:.1%}",
 		hovertemplate: "%{y}<br>%{x} (%{customdata:.1%})<extra></extra>",
-		customdata: bigBird["Win %"],
+		customdata: bird["Win %"].toArray(),
 		orientation: "h",
 		marker: {
 			color: "rgb(79, 195, 247)"
@@ -162,41 +166,41 @@ function commanderBar(data) {
 	}, {
 		displayModeBar: false
 	});
-
 	const chart = document.getElementById("commander-bar");
-	chart.on('plotly_click', function (data) {
-		const val = data.points[0].y;
-		bert = bert.filterby(row => row["Commander"] === val);
-		snackbar(val);
-		charts();
+	chart.on('plotly_click', function(data) {
+		console.log(data);
+		console.log(data.points);
+		console.log(data.points[0]);
+		console.log(data.points[0].pointIndex);
+		console.log(data.points[0].pointIndex);
+		const val = bird["Commander"][data.points[0].pointIndex];
+		filters.push(["Commander", val]);
+		displaySnackbar(val);
+		updateCharts();
 	});
 }
 
-function playerBar(data) {
-	let elmo = data.filterby(row => row["Player"] !== "Winner");
-	let abby = data.filterby(row => row["Player"] === "Winner");
-
-	let bigBird = new Birds({});
-	bigBird["playerValues"] = elmo.unique(["Name"]);
-	bigBird["playerCounts"] = bigBird["playerValues"].map(value => elmo.countif(["Name"], val => val === value));
-	bigBird["winnerCounts"] = bigBird["playerValues"].map(value => abby.countif(["Name"], val => val === value));
-	bigBird["loserCounts"] = bigBird.subtract(["playerCounts", "winnerCounts"])
-	bigBird["winPercent"] = bigBird.divide(['winnerCounts', 'playerCounts']);
-
-	bigBird = bigBird.sortby("playerValues", -1)
-	bigBird = bigBird.sortby("playerCounts", 1);
-	bigBird = bigBird.sortby("winnerCounts", 1);
-	bigBird = bigBird.tail(15);
-
-	maxGames = bigBird.max(["playerCounts"]);
-
+function updatePlayerBar() {
+	let playerData = second.filterby(row => row["Player"] !== "Winner");
+	let winnerData = second.filterby(row => row["Player"] === "Winner");
+	let bird = new Bird();
+	bird["Player"] = second["Name"].unique();
+	bird["# Games"] = bird["Player"].map(x => playerData["Name"].countif(y => y === x));
+	bird["# Wins"] = bird["Player"].map(x => winnerData["Name"].countif(y => y === x));
+	bird["# Losses"] = bird.sub(["# Games", "# Wins"]);
+	bird["Win %"] = bird.div(["# Wins", "# Games"]);
+	bird = bird.sortby("Player", -1);
+	bird = bird.sortby("# Games", 1);
+	bird = bird.sortby("# Wins", 1);
+	bird = bird.tail(15);
+	maxGames = bird["# Games"].max();
 	Plotly.newPlot("player-bar", [{
 		type: "bar",
-		x: bigBird["winnerCounts"],
-		y: bigBird["playerValues"],
+		x: bird["# Wins"].toArray(),
+		y: bird["Player"].toArray(),
 		name: "# Wins",
 		hovertemplate: "%{y}<br>%{x} (%{customdata:.1%})<extra></extra>",
-		customdata: bigBird["winPercent"],
+		customdata: bird["Win %"].toArray(),
 		orientation: "h",
 		marker: {
 			color: "rgb(3, 155, 229)"
@@ -208,14 +212,14 @@ function playerBar(data) {
 		}
 	}, {
 		type: "bar",
-		x: bigBird["loserCounts"],
-		y: bigBird["playerValues"],
+		x: bird["# Losses"].toArray(),
+		y: bird["Player"].toArray(),
 		name: "# Losses",
-		text: bigBird["winPercent"],
+		text: bird["Win %"].toArray(),
 		textposition: "outside",
 		texttemplate: "%{customdata:.1%}",
 		hovertemplate: "%{y}<br>%{x} (%{customdata:.1%})<extra></extra>",
-		customdata: bigBird["winPercent"],
+		customdata: bird["Win %"].toArray(),
 		orientation: "h",
 		marker: {
 			color: "rgb(79, 195, 247)"
@@ -258,42 +262,37 @@ function playerBar(data) {
 	}, {
 		displayModeBar: false
 	});
-
 	const chart = document.getElementById("player-bar");
-	chart.on('plotly_click', function (data) {
-		var val = data.points[0].y;
-		bert = bert.filterby(row => row["Name"] == val);
-		snackbar(val);
-		charts();
+	chart.on('plotly_click', function(data) {
+		const val = data.points[0].y;
+		filters.push(["Name", val]);
+		displaySnackbar(val);
+		updateCharts();
 	});
 }
 
-function archetypeCharts(data) {
-	let elmo = data.filterby(row => row["Player"] !== "Winner");
-	let abby = data.filterby(row => row["Player"] === "Winner");
-
-	let bird = new Birds({});
-	bird["playerValues"] = elmo.unique(["Archetype"]);
-	bird["playerCounts"] = bird["playerValues"].map(value => elmo.countif(["Archetype"], val => val === value));
-	bird["winnerCounts"] = bird["playerValues"].map(value => abby.countif(["Archetype"], val => val === value));
-	bird["playerPros"] = bird.weights(["playerCounts"]);
-	bird["winnerPros"] = bird.weights(["winnerCounts"]);
-	bird["winningness"] = bird.subtract(["winnerPros", "playerPros"]);
-
-	bird = bird.sortby('winningness');
-
-	const minWinningness = bird.min(['winningness']);
-	const maxWinningness = bird.max(['winningness']);
-
+function updateArchetypeCharts() {
+	let playerData = second.filterby(row => row["Player"] !== "Winner");
+	let winnerData = second.filterby(row => row["Player"] === "Winner");
+	let bird = new Bird();
+	bird["Archetype"] = playerData["Archetype"].unique();
+	bird["# Games"] = bird["Archetype"].map(x => playerData["Archetype"].countif(y => y === x));
+	bird["# Wins"] = bird["Archetype"].map(x => winnerData["Archetype"].countif(y => y === x));
+	bird["Game %"] = bird["# Games"].div(bird["# Games"].sum());
+	bird["Win %"] = bird["# Wins"].div(bird["# Wins"].sum());
+	bird["Winningness"] = bird.sub(["Win %", "Game %"]);
+	bird = bird.sortby("Winningness", 1);
+	const minWinningness = bird["Winningness"].min();
+	const maxWinningness = bird["Winningness"].max();
 	Plotly.newPlot("archetype-bar", [{
 		type: "bar",
-		x: bird["winningness"],
-		y: bird["playerValues"],
-		text: bird["winningness"],
+		x: bird["Winningness"].toArray(),
+		y: bird["Archetype"].toArray(),
+		text: bird["Winningness"].toArray(),
 		textposition: "outside",
 		texttemplate: "%{x:+.1%}",
 		hovertemplate: "%{y}<br>%{customdata} (%{x:+.1%})<extra></extra>",
-		customdata: bird["winnerCounts"],
+		customdata: bird["# Wins"].toArray(),
 		orientation: "h",
 		marker: {
 			color: "rgb(39, 170, 226)"
@@ -336,25 +335,21 @@ function archetypeCharts(data) {
 	}, {
 		displayModeBar: false
 	});
-
 	const bar = document.getElementById("archetype-bar");
-	bar.on('plotly_click', function (data) {
+	bar.on('plotly_click', function(data) {
 		var val = data.points[0].y;
-		bert = bert.filterby(row => row["Archetype"] == val);
-		snackbar(val);
-		charts();
+		filters.push(["Archetype", val]);
+		displaySnackbar(val);
+		updateCharts();
 	});
-
-	bird = bird.sortby("playerCounts", -1);
-
-	N = bird.size[0];
+	bird = bird.sortby("# Games", -1);
+	N = bird.length;
 	const [chartColors, textColors] = generateColors([2, 136, 209], [129, 212, 250], N);
-
 	Plotly.newPlot("archetype-pie", [{
 		type: "pie",
 		showlegend: false,
-		values: bird["playerCounts"],
-		labels: bird["playerValues"],
+		values: bird["# Games"].toArray(),
+		labels: bird["Archetype"].toArray(),
 		textposition: "outside",
 		texttemplate: "%{label}<br>%{percent:.1%}",
 		hovertemplate: "%{label}<br>%{value} (%{percent:.1%})<extra></extra>",
@@ -378,46 +373,40 @@ function archetypeCharts(data) {
 	}, {
 		displayModeBar: false
 	});
-
 	const pie = document.getElementById("archetype-pie");
 	pie.on('plotly_click', function (data) {
 		var val = data.points[0].label;
-		bert = bert.filterby(row => row["Archetype"] == val);
-		snackbar(val);
-		charts();
+		filters.push(["Archetype", val]);
+		displaySnackbar(val);
+		updateCharts();
 	});
 }
 
-function colorCharts(data) {
-	let elmo = data.filterby(row => row["Player"] !== "Winner");
-	let abby = data.filterby(row => row["Player"] === "Winner");
-
-	let bigBird = new Birds({});
-	bigBird["playerValues"] = data.unique(["Color"]);
-	bigBird["playerCounts"] = bigBird["playerValues"].map(value => elmo.countif(["Color"], val => val === value));
-	bigBird["winnerCounts"] = bigBird["playerValues"].map(value => abby.countif(["Color"], val => val === value));
-	bigBird["playerPros"] = bigBird.weights(["playerCounts"]);
-	bigBird["winnerPros"] = bigBird.weights(["winnerCounts"]);
-	bigBird["winningness"] = bigBird.subtract(["winnerPros", "playerPros"]);
-
-	bigBird = bigBird.sortby('winningness');
-
-	const minWinningness = bigBird.min(['winningness']);
-	const maxWinningness = bigBird.max(['winningness']);
-
-	const threshold = bigBird.sum(['playerCounts']) * 0.025; // feels good
-	const otherCount = bigBird.sumif(["playerCounts"], val => val <= threshold, ["playerCounts"]);
-	bigBird = bigBird.filterby(row => row["playerCounts"] > threshold);
-
+function updateColorCharts() {
+	let playerData = second.filterby(row => row["Player"] !== "Winner");
+	let winnerData = second.filterby(row => row["Player"] === "Winner");
+	let bird = new Bird();
+	bird["Color"] = playerData["Color"].unique();
+	bird["# Games"] = bird["Color"].map(x => playerData["Color"].countif(y => y === x));
+	bird["# Wins"] = bird["Color"].map(x => winnerData["Color"].countif(y => y === x));
+	bird["Game %"] = bird["# Games"].div(bird["# Games"].sum());
+	bird["Win %"] = bird["# Wins"].div(bird["# Wins"].sum());
+	bird["Winningness"] = bird.sub(["Win %", "Game %"]);
+	bird = bird.sortby("Winningness", 1);
+	const minWinningness = bird["Winningness"].min();
+	const maxWinningness = bird["Winningness"].max();
+	const threshold = bird["# Games"].sum() * 0.025; // Good number
+	const other = bird["# Games"].sumif(val => val <= threshold);
+	bird = bird.filterby(row => row["# Games"] >= threshold);
 	Plotly.newPlot("color-bar", [{
 		type: "bar",
-		x: bigBird["winningness"],
-		y: bigBird["playerValues"],
-		text: bigBird["winningness"],
+		x: bird["Winningness"].toArray(),
+		y: bird["Color"].toArray(),
+		text: bird["Winningness"].toArray(),
 		textposition: "outside",
 		texttemplate: "%{x:+.1%}",
 		hovertemplate: "%{y}<br>%{customdata} (%{x:+.1%})<extra></extra>",
-		customdata: bigBird["winnerCounts"],
+		customdata: bird["# Wins"].toArray(),
 		orientation: "h",
 		marker: {
 			color: "rgb(39, 170, 226)"
@@ -428,7 +417,12 @@ function colorCharts(data) {
 			font: { color: "black", family: "Arial" },
 		},
 	}], {
-		margin: { l: 75, r: 25, b: 25, t: 25 },
+		margin: {
+			l: 75,
+			r: 25,
+			b: 25,
+			t: 25,
+		},
 		xaxis: {
 			range: [
 				(6 * minWinningness - maxWinningness) / 5,
@@ -445,39 +439,34 @@ function colorCharts(data) {
 			dtick: 1,
 			mirror: "ticks",
 			linecolor: "black",
-			tickfont: { color: "white", family: "Arial" }
+			tickfont: { color: "white", family: "Arial" },
 		},
 		paper_bgcolor: "#1e1e1e",
 		plot_bgcolor: "#232323",
 		hovermode: "closest",
 		width: 480,
-		height: 300
+		height: 300,
 	}, {
 		displayModeBar: false
 	});
-
 	const bar = document.getElementById("color-bar");
-	bar.on('plotly_click', function (data) {
+	bar.on('plotly_click', function(data) {
 		var val = data.points[0].y;
-		bert = bert.filterby(row => row["Color"] == val);
-		snackbar(val);
-		charts();
+		filters.push(["Color", val]);
+		displaySnackbar(val);
+		updateCharts();
 	});
-
-	bigBird = bigBird.sortby("playerCounts", -1);
-	if (otherCount > 0) {
-		bigBird["playerValues"] = bigBird["playerValues"].concat("Other");
-		bigBird["playerCounts"] = bigBird["playerCounts"].concat(otherCount);
+	bird = bird.sortby("# Games", -1);
+	if (other > 0) {
+		bird.data.push({"Color": "Other", "# Games": other});
 	}
-
-	const N = bigBird.size[0];
-	const [chartColors, textColors] = generateColors([2, 119, 189], [179, 229, 252], N);
-
+	N = bird.length;
+	const [chartColors, textColors] = generateColors([2, 136, 209], [129, 212, 250], N);
 	Plotly.newPlot("color-pie", [{
 		type: "pie",
 		showlegend: false,
-		values: bigBird["playerCounts"],
-		labels: bigBird["playerValues"],
+		values: bird["# Games"].toArray(),
+		labels: bird["Color"].toArray(),
 		textposition: "outside",
 		texttemplate: "%{label}<br>%{percent:.1%}",
 		hovertemplate: "%{label}<br>%{value} (%{percent:.1%})<extra></extra>",
@@ -501,26 +490,14 @@ function colorCharts(data) {
 	}, {
 		displayModeBar: false
 	});
-
 	const pie = document.getElementById("color-pie");
 	pie.on('plotly_click', function (data) {
 		var val = data.points[0].label;
-		bert = bert.filterby(row => row["Color"] == val);
-		snackbar(val);
-		charts();
+		filters.push(["Color", val]);
+		displaySnackbar(val);
+		updateCharts();
 	});
-
 }
-
-/*
- ______ _    _ _   _  _____ _______ _____ ____  _   _  _____
-|  ____| |  | | \ | |/ ____|__   __|_   _/ __ \| \ | |/ ____|
-| |__  | |  | |  \| | |       | |    | || |  | |  \| | (___
-|  __| | |  | | . ` | |       | |    | || |  | | . ` |\___ \
-| |    | |__| | |\  | |____   | |   _| || |__| | |\  |____) |
-|_|     \____/|_| \_|\_____|  |_|  |_____\____/|_| \_|_____/
-
-*/
 
 /**
  * Generates a number of colors between two RGB values, inclusive.
